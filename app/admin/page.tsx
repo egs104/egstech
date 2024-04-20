af4c1f275ee6f1f6d4986a683294d5d730a7f1c0
Eric Suarez <egs104@gmail.com>
2024-04-20 11:28:04 -0700

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+/*
 import { auth } from 'app/auth';
 import { getGuestbookEntries } from 'app/db/queries';
 import { redirect } from 'next/navigation';
@@ -22,3 +23,4 @@ export default async function GuestbookPage() {
     </section>
   );
 }
+*/
